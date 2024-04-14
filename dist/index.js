6d9ea8d06af5a75933b068cddc9d69e9de029bb6
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-14 13:01:05 -0400

    Creacion del backend y configuracion base
    Vaa quedar muy bonita

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const nombre = 'Juan';
+console.log(nombre);
+console.log('Hola mundo');
+console.log('Hola mundo');
+console.log('Hola mundo');
---
+++ src/models/server.ts
@@ -0,0 +1,21 @@
+import express, { Application } from 'express';
+
+class Server{
+    private app: Application;
+    private port: string;
+
+    constructor(){
+        this.app = express();
+        this.port = '3001';
+        this.listen();   
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        })
+    }
+
+}
+
+export default Server;
